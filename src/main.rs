f2eb5394ff16fff8801daf620d0f1bc1dc1d4497
gfreezy <gfreezy@gmail.com>
2019-10-13 00:36:14 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -70,12 +70,12 @@ fn main() {
             )
             .await,
         );
-        let port = 53;
+        let port = 53usize;
 
         println!("Listening on port {}", port);
 
         // Start DNS servers
-        let udp_server = DnsUdpServer::new(resolver).await;
+        let udp_server = DnsUdpServer::new(0, resolver).await;
         udp_server.run_server().await
     })
 }
